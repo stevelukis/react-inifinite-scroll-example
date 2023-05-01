71f23cbb123c68668a2a794f76b528fd1a261120
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-05-01 10:10:11 +0800

    feat(ui): create grid for products

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { Box, Container, SimpleGrid } from "@chakra-ui/react";
 import ProductCard from "./ProductCard";
 
 const HomePage = () => {
+  const items = [...Array(20)];
+
   return (
-    <div>
-      <ProductCard />
-    </div>
+    <Container maxW="6xl" padding={5} >
+      <SimpleGrid columns={{ sm: 2, md: 5 }} gap={2}>
+        {items.map((_, i) => (
+          <ProductCard key={i} />
+        ))}
+      </SimpleGrid>
+    </Container>
   );
 };
 
